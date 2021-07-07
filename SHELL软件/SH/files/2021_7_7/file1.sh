200c307c3d6d002c2895a8a424ce09ec43e0661d
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-07-07 15:11:03 +0800

    Add files via upload

---
+++ SHELL软件/SH/files/2021_7_7/file1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+time=$(date +%F)
+directory=/tmp/$time
+if [ ! -d $directory ];then
+    mkdir $directory
+else
+    echo "该时间目录已存在，忽略！！"
+fi
+touch1=1.shell
+touch2=2.shell
+touch3=3.shell
+if [ ! -f $directory/$touch1 ];then
+    touch $directory/$touch1
+else
+    echo "该文件1.shell已存在，忽略！！"
+fi
+
+
