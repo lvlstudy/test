6ed69311e5b34cbe9832f9decbf5747b72b84cdf
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-07-10 23:47:30 +0800

    Add files via upload

---
+++ SHELL软件/SH/files/2021_7_7/pushssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+host1=`cat /etc/ansible/hosts | awk -F: ""'{print $1}' | grep '^192'`
+for i in $host1
+do
+  scp ~/.ssh/authorized_keys root@$i:~/.ssh/authorized_keys
+  password="123"
+  /usr/bin/expect -c"
+    spawn ssh-copy-id root@$i
+    expect {
+    \"*password\"{send\"$password\r\";exp_continue}
+    }
+  expect eof"
+done
+
