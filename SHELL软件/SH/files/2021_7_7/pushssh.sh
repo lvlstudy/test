859e6eae668497ba3530880d58df08b52d9771d5
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-07-10 23:52:51 +0800

    Update pushssh.sh

--- SHELL软件/SH/files/2021_7_7/pushssh.sh
+++ SHELL软件/SH/files/2021_7_7/pushssh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#前提要产生公私钥
+#前提要下载expect这个Linux软件包，好产生expect模块
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 host1=`cat /etc/ansible/hosts | awk -F: ""'{print $1}' | grep '^192'`
 for i in $host1
