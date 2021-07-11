d678f02527d21479c8d0653b5c1dcfd0d2ef3a45
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-07-11 09:10:48 +0800

    Update pushssh.sh

--- SHELL软件/SH/files/2021_7_7/pushssh.sh
+++ SHELL软件/SH/files/2021_7_7/pushssh.sh
@@ -2,6 +2,16 @@
 #前提要产生公私钥
 #前提要下载expect这个Linux软件包，好产生expect模块
 #批量传公私钥，实现无密码登录
+#产生密钥对验证
+ssh-keygen -t rsa
+#ssh协议免交互代理
+ssh-agent bash
+ssh-add
+
+#shell脚本批量发送公钥
+yum install expect -y
+#cd ~/.ssh
+#vim ~/.ssh/pushssh.sh
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 host1=`cat /etc/ansible/hosts | awk -F: ""'{print $1}' | grep '^192'`
 for i in $host1
