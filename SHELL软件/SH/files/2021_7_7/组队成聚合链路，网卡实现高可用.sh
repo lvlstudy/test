8fafd9f03e5763578753a0031dc4b35023db5a41
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-07-11 18:41:13 +0800

    Update 组队成聚合链路，网卡实现高可用.sh

--- SHELL软件/SH/files/2021_7_7/组队成聚合链路，网卡实现高可用.sh
+++ SHELL软件/SH/files/2021_7_7/组队成聚合链路，网卡实现高可用.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #准备环境：在原有的虚拟机上新添加两个网卡，然后将他们配置成team,接着测试虚拟设备team网卡是否真的高可用？@@@
 #发现：在一开始eth1和eth2址为192.168.202.9和192.168.202.10,接着配了team0之后两地址就消失了，地址也只有改完的team0的ip地址；这说明两物理网卡都被加到了虚拟设备虚拟网卡team0中
+#man teamd.conf              #查看帮助信息，找到EXAMPLE下，有备份方式，复制
 nmcli connection add type team con-name team0 ifname team0 autoconnect yes config '{"runner": {"name": "activebackup"}}'
 #添加team成员即奴隶
 nmcli connection add type team-slave con-name team0-1 ifname eth1 master team0
