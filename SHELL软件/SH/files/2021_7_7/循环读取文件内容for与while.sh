062846548d4a10280a1f2bde5a1d05be79d9fb00
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-08-17 23:19:19 +0800

    Add files via upload

---
+++ SHELL软件/SH/files/2021_7_7/循环读取文件内容for与while.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+while read line
+do
+  echo $line
+#while使用输入重定向以保证从文件中读取数据
+done < name.txt
+
+
+for x in  $(<name.txt) 
+do
+  echo $x
+done
+
+
+
+for x in `cat name.txt`
+do
+  echo $x
+done
