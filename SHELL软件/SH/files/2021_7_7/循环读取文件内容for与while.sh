37b336a6caade15e16495f551e6f339728338c60
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-08-18 00:00:40 +0800

    Update 循环读取文件内容for与while.sh

--- SHELL软件/SH/files/2021_7_7/循环读取文件内容for与while.sh
+++ SHELL软件/SH/files/2021_7_7/循环读取文件内容for与while.sh
@@ -3,6 +3,12 @@
 while read line
 do
   echo $line
+  ping -c 2 $line >>/dev/null
+  if [ $? = 0 ];then
+     echo "$line is up" >>ping_ok.log
+   else
+      echo "$line is down" >>ping_error.log
+   fi
 #while使用输入重定向以保证从文件中读取数据
 done < name.txt
 
