46675b4fecd47df5ef658b091b1219fc0d1063dd
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2021-08-23 14:20:39 +0800

    Update 主从同步之主服务器脚本.sh

--- 数据库/mysql/主从同步/主从同步之主服务器脚本.sh
+++ 数据库/mysql/主从同步/主从同步之主服务器脚本.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #时间: 2021/08/23
 #设置数据库主服务器
+#给配置文件/etc/my.cnf添加应有的参数
 server_id=`grep "server-id=1" /etc/my.cnf`
 log_bin=`grep "log_bin=master-bin" /etc/my.cnf`
 log_bin_index=`grep "log_bin_index=master-bin.index" /etc/my.cnf`
@@ -21,7 +22,10 @@ if [ -z "$binlog_do_db" ]
 then
    echo "binlog_do_db=test" >>/etc/my.cnf
 fi
+#添加授权用户
 mysql --connect-expired-password -uroot -p'123456' -e "grant replication slave on *.* to backup@'192.168.8.%' identified by 'Zjq?123456'"
+#重启数据库服务使得配置生效
 systemctl restart mysqld
+#进入数据库查询主服务器的状态
 mysql --connect-expired-password -uroot -p123456 -e "show master status"
 
