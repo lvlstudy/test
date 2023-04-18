61ae8c114e5457915de27d34947bccf31a67d4fa
lvlstudy <87048256+lvlstudy@users.noreply.github.com>
2023-04-18 14:47:48 +0800

    Update and rename 清空大文件脚本 to 清空大文件脚本cipan.sh

--- 练习脚本/清空大文件脚本cipan.sh
+++ 练习脚本/清空大文件脚本cipan.sh
@@ -1,3 +1,4 @@
+第一个脚本
 #!/bin/bash
 #大文件所在位置
 maxfile=/tmp/test.cst
@@ -15,4 +16,23 @@ else
    echo "$maxfile大小尺寸为`du -m $maxfile |awk '{print $1"M"}'`"
    echo "无需清空文件，文件大小不大"  
 fi
+##########################################################################################################################
+第二个脚本
+#!/bin/bash
+PWD=/tmp/jiaoben
+file=rumenz.img
+Sourcefile=$PWD/$file
+Num=`du -s $Sourcefile|awk '{print $1}'`
+Size=`echo $((2*1000*1000))`
+if [ -e "$Sourcefile" ] &&  [ $Num -ge $Size ]
+then
+  echo " " > $Sourcefile 
+elif [ ! -e "$Sourcefile" ]
+then
+    echo "$Sourcefile not exist"
+else
+  echo "$Sourcefile'file size ok"
+fi
 
+
+  
